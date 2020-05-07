426c9ee233599ba8d41145db9debef77ae94b623
kangxufeng <530039987@qq.com>
2020-05-07 14:13:27 +0800

    groupAnagrams

---
+++ 49. Group Anagrams/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+  let str = strs[0]
+  console.log(str.split('').sort().)
+};
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
