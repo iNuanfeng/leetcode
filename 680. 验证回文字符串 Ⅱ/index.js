f385405e2a136696949e5858d1db0d8529c16ace
kangxufeng <530039987@qq.com>
2020-05-19 14:51:47 +0800

    双指针判断回文：680. 验证回文字符串 Ⅱ

---
+++ 680. 验证回文字符串 Ⅱ/index.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
+
+
+  function isPali(s, l, r) { // 辅助函数
+    while (l < r) { // 指针相遇 结束循环
+      if (s[l] !== s[r]) { // 一票否决
+        return false 
+      }
+      l++ // 指针挪动，相互逼近
+      r--
+    }
+    return true // 没有遇到不同，返回true
+  }
+  let l = 0, r = s.length - 1 // 创建双指针
+    while (l < r) { 
+      if (s[l] !== s[r]) { // 转为判断删掉左右指针字符之一的字串，是否是回文串
+        return isPali(s, l + 1, r) || isPali(s, l, r - 1)
+      }
+      l++
+      r--
+    }
+    return true
+};
+
+console.log(validPalindrome('aba'))
